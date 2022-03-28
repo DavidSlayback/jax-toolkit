f20168db72beee8b529b08ac75c417eab42ced23
David Slayback <slayback.d@northeastern.edu>
2022-03-28 11:51:06 -0400

    Added fake GRU class

--- jax_toolkit/networks/rnn/gru.py
+++ jax_toolkit/networks/rnn/gru.py
@@ -1,4 +1,4 @@
-__all__ = ['LearnableGRU', 'LearnableLayerNormGRU', 'LayerNormGRU', 'GRU', 'get_gru_cls']
+__all__ = ['LearnableGRU', 'LearnableLayerNormGRU', 'LayerNormGRU', 'GRU', 'get_gru_cls', 'get_gru_cls_or_fake']
 
 from functools import partial
 from typing import Optional, Callable
@@ -61,3 +61,25 @@ def get_gru_cls(ln: bool = False, learnable: bool = False, orthogonal_init: bool
     else: cls = LearnableGRU if learnable else hk.GRU
     if orthogonal_init: cls = partial(cls, w_i_init=get_orthogonal_activation('sigmoid'), w_h_init=get_orthogonal_activation('sigmoid'), b_init=jnp.zeros)
     return cls
+
+
+def get_gru_cls_or_fake(recurrent: bool = True, ln: bool = False, learnable: bool = False, orthogonal_init: bool = True) -> Callable:
+    """Either get appropriate GRU class or a Linear layer that behaves similarly"""
+    if recurrent: return get_gru_cls(ln, learnable, orthogonal_init)
+    class FakeGRU(hk.Module):
+        def __init__(self, hidden_size: int,
+                     w_i_init: Optional[hk.initializers.Initializer] = get_orthogonal_activation('tanh') if orthogonal_init else None,
+                     w_h_init: Optional[hk.initializers.Initializer] = None,
+                     b_init: Optional[hk.initializers.Initializer] = jnp.zeros if orthogonal_init else None,
+                     ln: bool = False,
+                     name: Optional[str] = None,):
+            super().__init__(name=None)
+            self._ln = ln
+            self._linear = hk.Linear(hidden_size, True, w_init=w_i_init, b_init=b_init)
+
+        def __call__(self, inputs, state):
+            out = self._linear(inputs)
+            if self._ln: out = hk.LayerNorm(-1, True, True)(out)
+            out = jax.nn.tanh(out)
+            return out, out
+    return FakeGRU
